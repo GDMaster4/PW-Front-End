d58131db6dac20d72adfb6be0feae4eb1a5ddb97
GDMaster4 <dalmasogianluca@gmail.com>
2024-09-30 12:20:29 +0200

    linea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,5 +19,18 @@ export class AppComponent
     this.authSrv.logout()
   }
 
+  premuto(event:Event)
+  {
+    const clicked= event.target as HTMLElement;
+    clicked.style.textDecoration="underline";
+    const links= document.getElementsByClassName("nav-link text-white");
+    for (let i = 0; i < links.length; i++)
+    {
+      const link= links[i] as HTMLElement;
+      if (link !== clicked) {
+        link.style.textDecoration = "none"; // Aggiungi decorazione del testo
+      }
+    }
+  }
   
 }
